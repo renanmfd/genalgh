f10a8687dbbad0ce6d99db288a790ac75ffa55d8
Renan Dias <rdias@cbitsgroup.com>
2017-06-28 18:30:59 -0300

    Rules

--- core/attributes/rules/RuleBase.php
+++ core/attributes/rules/RuleBase.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @file RuleInterface.php
+ * @file RuleBase.php
  * 
  * @package Evolution\attributes\rules;
  */
@@ -8,22 +8,35 @@
 namespace Evolution\attributes\rules;
 
 /**
- * @interface RuleInterface
+ * @interface RuleBase
  */
-interface RuleInterface {
+abstract class RuleBase extends RuleInterface {
+
+  protected $id;
+
+  /**
+   *
+   */
+  public function __construct() {
+    $this->id = uniqid("");
+  }
 
   /**
    * Get the number of identification of the rule.
    *
    * @return integer The number of identification of this rule.
    */
-  public function getId();
+  public function getId() {
+    return $this->id;
+  }
 
   /**
    * Validate the attribute value.
    *
    * @return bool True if the attribute value is valid, false otherwise.
    */
-  public function validate($attribute_value);
+  public function validate($attribute_value) {
+    
+  }
 
 }
